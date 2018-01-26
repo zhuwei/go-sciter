03cf49608f2b2994c41b5d79a5a24ad9b317c222
kerr <zhudan622@163.com>
2018-01-26 13:23:04 +0800

    Update fixedWindow.go

--- examples/fixed/fixedWindow.go
+++ examples/fixed/fixedWindow.go
@@ -4,8 +4,8 @@ import (
 	"flag"
 	"log"
 
-	"github.com/sciter-sdk/go-sciter"
-	"github.com/sciter-sdk/go-sciter/window"
+	"github.com/zhuwei/go-sciter"
+	"github.com/zhuwei/go-sciter/window"
 )
 
 func main() {
